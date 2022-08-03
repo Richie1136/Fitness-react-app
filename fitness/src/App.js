d4f9fc088e3821eef60182b0e9a4b2bc11652628
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-03 11:15:29 -0400

    Finishing up basic strucutre for the app.js file

--- fitness/src/App.js
+++ fitness/src/App.js
@@ -4,15 +4,17 @@ import { Box } from '@mui/material';
 import NavBar from './components/navbar/NavBar';
 import Home from './components/home/Home';
 import ExcerciseDetail from './components/excercisedetail/ExcerciseDetail';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
       <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/exercise/:id' element={<ExcerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 }
