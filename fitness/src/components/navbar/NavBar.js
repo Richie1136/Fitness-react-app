5e0d1f742335de57bb1cdc5af3c93a24e5748f1b
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-03 11:31:25 -0400

    Building the navBar

--- fitness/src/components/navbar/NavBar.js
+++ fitness/src/components/navbar/NavBar.js
@@ -1,7 +1,20 @@
+import { NavLink } from 'react-router-dom'
+import { Stack } from '@mui/material'
+import Logo from '../../assets/images/Logo2.png'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <Stack>
+      <NavLink to='/'>
+        <img src={Logo} alt='logo' style={{ width: '48px', height: '48px', margin: '0 20px' }} />
+      </NavLink>
+      <Stack>
+        <NavLink style={{ 'textDecoration': 'none', 'color': '#3A1212', 'borderBottom': '3px solid #FF2625' }} to='/home'>
+          <span>Home</span>
+        </NavLink>
+        <a href="#exercises" style={{ 'textDecoration': 'none', 'color': '#3A1212' }}>Exercises</a>
+      </Stack>
+    </Stack>
   )
 }
 
