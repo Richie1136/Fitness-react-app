3d73008c7777417c85f2b79218b39a6a55b8c56a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-03 10:57:36 -0400

    Created routes inside of the app component

--- fitness/src/App.js
+++ fitness/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material';
+import NavBar from './components/navbar/NavBar';
+import Home from './components/home/Home';
+import ExcerciseDetail from './components/excercisedetail/ExcerciseDetail';
 
 function App() {
   return (
-    <div>
-      <h2>Fitness app</h2>
-    </div>
+    <Box width="400px">
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExcerciseDetail />} />
+      </Routes>
+    </Box>
   );
 }
 
