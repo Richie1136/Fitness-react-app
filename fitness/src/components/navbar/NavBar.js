4ccc090fa56aab0a7dcb4e954086e6424a8d1497
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-03 11:40:33 -0400

    Added more styling to the navBar

--- fitness/src/components/navbar/NavBar.js
+++ fitness/src/components/navbar/NavBar.js
@@ -4,11 +4,11 @@ import Logo from '../../assets/images/Logo2.png'
 
 const NavBar = () => {
   return (
-    <Stack>
+    <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '122px', xs: "40px" }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
       <NavLink to='/'>
         <img src={Logo} alt='logo' style={{ width: '48px', height: '48px', margin: '0 20px' }} />
       </NavLink>
-      <Stack>
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
         <NavLink style={{ 'textDecoration': 'none', 'color': '#3A1212', 'borderBottom': '3px solid #FF2625' }} to='/home'>
           <span>Home</span>
         </NavLink>
