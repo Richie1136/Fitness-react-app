b92a03e911d61e532c40f31b06090e07c73a9287
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-04 11:19:40 -0400

    Wrapping the Content insdie of the HorizontalScrollBar component in the ScrollMenu package from react-horizontal-scrolling-menu

--- fitness/src/components/horizontalscrollbar/HorizontalScrollBar.js
+++ fitness/src/components/horizontalscrollbar/HorizontalScrollBar.js
@@ -1,15 +1,16 @@
 import { Box } from "@mui/material"
 import BodyPart from "../bodypart/BodyPart"
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu"
 
 const HorizontalScrollBar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <div>
+    <ScrollMenu>
       {data.map((item) => (
         <Box key={item.id} itemId={item.id} title={item.id} m="0 40px">
           <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
         </Box>
       ))}
-    </div>
+    </ScrollMenu>
   )
 }
 
