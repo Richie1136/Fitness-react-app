8ee538ea724e7c790f2dac431f0298fcf907f454
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-04 11:15:50 -0400

    Cleaning up the styling code

--- fitness/src/components/bodypart/BodyPart.js
+++ fitness/src/components/bodypart/BodyPart.js
@@ -4,27 +4,18 @@ import Icon from '../../assets/icons/gym.png'
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
     <Stack className="bodyPart-card" type="button" alignItems="center" justifyContent="center"
-      sx={
-        bodyPart === item ? {
-          'borderTop': '4px solid #ff2625',
-          'backgroundColor': '#fff',
-          'borderBottomLeftRadius': '20px',
-          'width': '270px',
-          'height': '280px',
-          'cursor': 'pointer',
-          'gap': '47px',
-        } : {
-          'backgroundColor': '#fff',
-          'borderBottomLeftRadius': '20px',
-          'width': '270px',
-          'height': '280px',
-          'cursor': 'pointer',
-          'gap': '47px',
-        }
-      }
+      sx={{
+        borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+        'backgroundColor': '#fff',
+        'borderBottomLeftRadius': '20px',
+        'width': '270px',
+        'height': '280px',
+        'cursor': 'pointer',
+        'gap': '47px',
+      }}
     >
       <img style={{ 'width': '40px', 'height': '40px' }} src={Icon} alt='icon' />
-    </Stack >
+    </Stack>
   )
 }
 
