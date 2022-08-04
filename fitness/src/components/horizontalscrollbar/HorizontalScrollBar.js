be7c56efdfa831496e33ee816fde4b83ceee0ccf
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-04 11:38:08 -0400

    Added left and right arrows

--- fitness/src/components/horizontalscrollbar/HorizontalScrollBar.js
+++ fitness/src/components/horizontalscrollbar/HorizontalScrollBar.js
@@ -1,17 +1,47 @@
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import BodyPart from "../bodypart/BodyPart"
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu"
+import { useContext } from "react"
+import LeftArrowIcon from '../../assets/icons/left-arrow.png'
+import RightArrowIcon from '../../assets/icons/right-arrow.png'
 
-const HorizontalScrollBar = ({ data, bodyPart, setBodyPart }) => {
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  const handleScrollPrev = () => {
+    scrollPrev()
+  }
+
   return (
-    <ScrollMenu>
-      {data.map((item) => (
-        <Box key={item.id} itemId={item.id} title={item.id} m="0 40px">
-          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-        </Box>
-      ))}
-    </ScrollMenu>
-  )
-}
+    <Typography onClick={handleScrollPrev} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
 
-export default HorizontalScrollBar
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  const handleScrollNext = () => {
+    scrollNext()
+  }
+
+  return (
+    <Typography onClick={handleScrollNext} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    {data.map((item) => (
+      <Box key={item.id} itemId={item.id} title={item.id} m="0 40px">
+        <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      </Box>
+    ))}
+  </ScrollMenu>
+);
+
+export default HorizontalScrollbar;
