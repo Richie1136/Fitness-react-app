1426d25bd4f39fef1e50b08cc38de83cd126a5e0
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-05 13:29:15 -0400

    Importing helper data from fetchData file and importing package from mui material

--- fitness/src/components/excercisedetail/ExcerciseDetail.js
+++ fitness/src/components/excercisedetail/ExcerciseDetail.js
@@ -1,3 +1,8 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { Box } from "@mui/material"
+
+import { fetchData, exerciseOptions, baseURL } from '../../utils/fetchData'
 
 const ExcerciseDetail = () => {
   return (
