7b7b90d3fda56b494c2d12f4a5d36dcd107d3a9d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-05 11:58:55 -0400

    Created a currentPage state and a exerciseper page variable as well as a paginate function that will set the current page to the value and scroll to the top of the page

--- fitness/src/components/exercises/Exercises.js
+++ fitness/src/components/exercises/Exercises.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { Pagination } from "@mui/material/Pagination"
+import { Pagination } from "@mui/material"
 import { Box, Stack, Typography } from "@mui/material"
 import ExerciseCard from "../exercisecard/ExerciseCard"
 
@@ -7,7 +7,17 @@ import { exerciseOptions, fetchData, baseURL } from "../../utils/fetchData"
 
 
 const Exercises = ({ setExercises, bodyPart, exercises }) => {
-  console.log(exercises)
+
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const exercisesPerPage = 9
+
+  const paginate = (event, value) => {
+    setCurrentPage(value)
+
+    window.scrollTo({ top: 1800, behavior: 'smooth' })
+  }
+
   return (
     <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
       <Typography variant="h3" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
@@ -15,6 +25,18 @@ const Exercises = ({ setExercises, bodyPart, exercises }) => {
         {exercises.map((exercise, idx) => (
           <ExerciseCard key={idx} exercise={exercise} />
         ))}
+      </Stack>
+      <Stack mt='100px' alignItems="center">
+        {exercises?.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
       </Stack>
     </Box>
   )
