77ada7e2143cdd468ade5b7b6afa37c5f2c71a7e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-05 11:38:52 -0400

    Added 2 buttoons to display the body part and the target muscle underneath the gif

--- fitness/src/components/exercisecard/ExerciseCard.js
+++ fitness/src/components/exercisecard/ExerciseCard.js
@@ -4,6 +4,14 @@ import { Button, Stack, Typography } from "@mui/material"
 const ExerciseCard = ({ exercise }) => (
   <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
     <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+    <Stack direction='row'>
+      <Button sx={{ ml: '21px', color: '#fff', backgroundColor: '#ffa9a9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+        {exercise?.bodyPart}
+      </Button>
+      <Button sx={{ ml: '21px', color: '#fff', backgroundColor: '#fcc757', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+        {exercise?.target}
+      </Button>
+    </Stack>
   </Link>
 );
 
