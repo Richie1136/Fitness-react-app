772f35df54f4c320c32f46a1c582df2372786d92
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-05 13:10:48 -0400

    Mapping over the currentExercise instead of all of the excerises to only display the first 9 excerises

--- fitness/src/components/exercises/Exercises.js
+++ fitness/src/components/exercises/Exercises.js
@@ -28,7 +28,7 @@ const Exercises = ({ setExercises, bodyPart, exercises }) => {
     <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
       <Typography variant="h3" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
       <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
-        {exercises.map((exercise, idx) => (
+        {currentExercise.map((exercise, idx) => (
           <ExerciseCard key={idx} exercise={exercise} />
         ))}
       </Stack>
