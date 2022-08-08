3e97370399133a4dfbd01eac607df449e45a2490
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-08 13:59:50 -0400

    Rendering out the different exercises in the same target muscle group as well as rendering exercises using the same equipment

--- fitness/src/components/similarexercises/SimilarExercises.js
+++ fitness/src/components/similarexercises/SimilarExercises.js
@@ -1,10 +1,29 @@
 import { Box, Stack, Typography } from "@mui/material"
+import HorizontalScrollbar from "../horizontalscrollbar/HorizontalScrollBar"
+import Loading from "../loading/Loading"
 
 
 const SimilarExercises = ({ targetMuscleData, equipmentData }) => {
 
   return (
-    <div>SimilarExercises</div>
+    <Box sx={{ mt: { lg: '100px', xs: '0' } }}>
+      <Typography variant='h3'>
+        Exercises that target the same muscle group
+        <Stack direction='row' sx={{ p: '2px', position: 'relative' }}>
+          {targetMuscleData.length ?
+            <HorizontalScrollbar data={targetMuscleData} />
+            : <Loading />}
+        </Stack>
+      </Typography>
+      <Typography variant='h3'>
+        Exercises that target the same equipment group
+        <Stack direction='row' sx={{ p: '2px', position: 'relative' }}>
+          {equipmentData.length ?
+            <HorizontalScrollbar data={equipmentData} />
+            : <Loading />}
+        </Stack>
+      </Typography>
+    </Box>
   )
 }
 
